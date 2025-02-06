abd077d9e80b1882c2f3c22bffa4e4b5f0c8898b
suyeon0131 <suyeon0131@ajou.ac.kr>
2025-02-06 18:11:45 +0900

    Feat: 스프링 컨테이너

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,11 +1,19 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AppConfig에 있는 환경 설정 정보를 가지고 스프링 컨테이너에 다 넣어서 관리함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         // id가 1L인 이유는 Long 타입이어서 (안 붙이면 오류남)
         memberService.join(member);
