a994ec45f61699f189dac7443f331de0ee4d8846
suyeon0131 <suyeon0131@ajou.ac.kr>
2025-02-12 01:21:59 +0900

    Feat: 필터

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan( // 스프링 빈 자동 등록 (AppConfig가 자동으로 등록되지 않도록 제외 설정)
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
